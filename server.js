50c77785f66870f29b11d45f2f42277c3d925efa
eslamelmasry1 <89648925+eslamelmasry1@users.noreply.github.com>
2021-12-14 17:31:45 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,52 @@
+// Setup empty JS object to act as endpoint for all routes
+const projectData = [];
+
+// Require Express to run server and routes
+const express = require('express')
+// Start up an instance of app
+const app = express();
+//Including body-parser
+const bodyParser = require('body-parser');
+//Port to run the server on
+const port = 3330;
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+// Spin up the server
+const server = app.listen(port, () => {
+    console.log(`Running on http://localhost:${port}`)
+});
+
+//GET route with a function to complete GET '/all'
+app.get('/all', function(req, res) {
+    res.send(projectData)
+});
+
+//The POST route Setup
+//Create post() with a URL path and a callback function.
+app.post('/postData', postData)
+
+//Call back function in which we add the data received from req.body
+function postData(req, res) {
+    //Object with the required data
+    weatherData = {
+        temp: req.body.temp,
+        date: req.body.date,
+        content: req.body.content
+    };
+    //Push allData object to projectData object as endpoint.
+    projectData.push(weatherData);
+    res.send(projectData);
+    console.log(projectData);
+}
